f97844f30f169558240c9a631b7b467f38adc030
rafaelanobre <raafanobre@gmail.com>
2023-04-13 17:44:45 -0300

    logica js e tela login bonus

---
+++ scripts.js
@@ -0,0 +1,68 @@
+axios.defaults.headers.common['Authorization'] = 'Da3XNfDHx4X7NJWZEq9v7eO1';
+
+function enterUsername(){
+    //criar objeto user com o rótulo name
+    //pegar o nome do input e colocar no objeto
+    //declarar a variavel da requisição
+    //enviar para o sevidor com api const
+
+    //se retornar status 200:
+    //chamar a função renderMessages com then
+    //dar hidden na página de carregando
+    //tirar o hidden do header, main e footer
+
+    //se retornar status 400:
+    //chamar a função de erro com o catch
+
+}
+
+function renderMessages(){
+    //declarar a variavel da requisição
+    //buscar mensagens antigas com o api get
+    //criar um loop for que renderiza cada mensagem do array separada
+    //usar if para ver se é status ou mensagem
+
+    //usar o código abaixo enquanto renderiza
+    //const elementoQueQueroQueApareca = document.querySelector('.mensagem');
+    //elementoQueQueroQueApareca.scrollIntoView();
+}
+
+function newUsername(){
+    //alert de escolher um novo nome de usuário
+    //reload página para zerar usando
+    //window.location.reload()
+}
+
+function sendMessage(){
+    //criar um objeto mensagem
+    //pegar o texto para o objeto do input
+    //exibir se está enviando público ou privado
+
+    //declarar a variavel da requisição
+    //enviar para o sevidor com api const
+    //chamar newMessages
+}
+
+function enter(){
+    //ao clicar no enter
+    //chama a funcao sendmessage
+}
+
+function newMessages(){
+    //igual a função renderMessages
+    //mas essa vai só adicionar as novas mensagens no innerhtml
+}
+//usar set interval 3000
+setInterval(newMessages, 3000);
+
+
+function loginChat(){
+    //usar post para a url com o usuário
+}
+//usar set interval 5000
+setInterval(loginChat, 5000);
+
+
+//Bônus: Layout menu lateral
+//Bônus: API Buscar participantes
+
